4be8d570ebbdd0ee16e88fa8613b8f7cadff609f
Ryan SU <1137661202@qq.com>
2022-10-30 18:43:32 +0800

    fix: api model index

--- rpc/model/api.go
+++ rpc/model/api.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type Api struct {
 	gorm.Model
-	Path        string `json:"path" gorm:"comment:API path"`                    // API path | API 路径
-	Description string `json:"description" gorm:"comment:API description"`      // API description | API 描述
-	ApiGroup    string `json:"apiGroup" gorm:"comment:API group"`               // API group | API 分组
-	Method      string `json:"method" gorm:"default:POST;comment: HTTP method"` // HTTP method | HTTP 请求类型
+	Path        string `json:"path" gorm:"index:idx_path,priority:1;comment:API path"`                    // API path | API 路径
+	Description string `json:"description" gorm:"comment:API description"`                                // API description | API 描述
+	ApiGroup    string `json:"apiGroup" gorm:"comment:API group"`                                         // API group | API 分组
+	Method      string `json:"method" gorm:"default:POST;index:idx_path,priority:2;comment: HTTP method"` // HTTP method | HTTP 请求类型
 }
