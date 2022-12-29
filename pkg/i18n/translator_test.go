1581cc8c7de660f8b7aabdab42c24bdcdc2b9677
Ryan SU <1137661202@qq.com>
2022-12-29 12:26:18 +0800

    fix: translator test

--- pkg/i18n/translator_test.go
+++ pkg/i18n/translator_test.go
@@ -1,13 +1,15 @@
 package i18n
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTranslator(t *testing.T) {
 	l := &Translator{}
 	l.NewBundle(LocaleFS)
+	l.NewTranslator()
 	res := l.Trans("zh", "login.userNotExist")
-	fmt.Println(res)
+	assert.Equal(t, "用户不存在", res)
 }
