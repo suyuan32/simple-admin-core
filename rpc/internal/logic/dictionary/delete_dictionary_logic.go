38bc3d07bb6bcd6d7627728c6a78a95e3e534363
Ryan SU <1137661202@qq.com>
2023-03-01 08:27:23 +0800

    fix: delete dictionary with its details

--- rpc/internal/logic/dictionary/delete_dictionary_logic.go
+++ rpc/internal/logic/dictionary/delete_dictionary_logic.go
@@ -3,7 +3,10 @@ package dictionary
 import (
 	"context"
 
+	"github.com/suyuan32/simple-admin-core/pkg/ent"
 	"github.com/suyuan32/simple-admin-core/pkg/ent/dictionary"
+	"github.com/suyuan32/simple-admin-core/pkg/ent/dictionarydetail"
+	"github.com/suyuan32/simple-admin-core/pkg/utils"
 	"github.com/suyuan32/simple-admin-core/pkg/utils/errorhandler"
 	"github.com/suyuan32/simple-admin-core/rpc/internal/svc"
 	"github.com/suyuan32/simple-admin-core/rpc/types/core"
@@ -28,7 +31,19 @@ func NewDeleteDictionaryLogic(ctx context.Context, svcCtx *svc.ServiceContext) *
 }
 
 func (l *DeleteDictionaryLogic) DeleteDictionary(in *core.IDsReq) (*core.BaseResp, error) {
-	_, err := l.svcCtx.DB.Dictionary.Delete().Where(dictionary.IDIn(in.Ids...)).Exec(l.ctx)
+	err := utils.WithTx(l.ctx, l.svcCtx.DB, func(tx *ent.Tx) error {
+		_, txErr := tx.DictionaryDetail.Delete().Where(dictionarydetail.HasDictionariesWith(dictionary.IDIn(in.Ids...))).Exec(l.ctx)
+		if txErr != nil {
+			return txErr
+		}
+
+		_, txErr = tx.Dictionary.Delete().Where(dictionary.IDIn(in.Ids...)).Exec(l.ctx)
+		if txErr != nil {
+			return txErr
+		}
+
+		return nil
+	})
 	if err != nil {
 		return nil, errorhandler.DefaultEntError(err, in)
 	}
