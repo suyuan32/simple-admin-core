4300416ba8e6c8bf93903d6aa22599d10892d8c2
Ryan Su <yuansu.china.work@gmail.com>
2023-04-22 17:09:18 +0800

    docs: update version in swagger doc

--- api/core.go
+++ api/core.go
@@ -5,7 +5,7 @@
 //		Schemes: http, https
 //		Host: localhost:9100
 //		BasePath: /
-//		Version: 1.0.0
+//		Version: 1.0.2
 //		Contact: yuansu.china.work@gmail.com
 //		SecurityDefinitions:
 //		  Token:
