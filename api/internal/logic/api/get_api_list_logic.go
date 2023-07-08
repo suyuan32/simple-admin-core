08533232eac859b52474930456eca494d9d5a92e
Ryan SU <yuansu.china.work@gmail.com>
2023-07-08 08:36:30 +0800

    feat: reset password by email and sms

--- api/internal/logic/api/get_api_list_logic.go
+++ api/internal/logic/api/get_api_list_logic.go
@@ -43,18 +43,14 @@ func (l *GetApiListLogic) GetApiList(req *types.ApiListReq) (resp *types.ApiList
 	resp.Msg = l.svcCtx.Trans.Trans(l.ctx, i18n.Success)
 	resp.Data.Total = data.GetTotal()
 
-	// 接口分组翻译
+	// Translate api group
 	var group = make(map[string]string)
 	for _, v := range data.Data {
-		// 在角色管理页面调用该接口时为了获取所有接口，每页大小被设置为10000
-		// 因此可以根据每页数据大小来判断，该请求是否为来自角色管理页面
-		// 如果是来自API管理的请求则不去获取分组翻译，直接显示原本字段
+		// If page size is over 1000, use api group i18n translation. Mainly use in authority list.
 		if req.PageSize > 1000 {
-			// 检测是否存在分组翻译，避免多次获取
 			if _, exist := group[*v.ApiGroup]; !exist {
 				group[*v.ApiGroup] = l.svcCtx.Trans.Trans(l.ctx, "apiGroup."+*v.ApiGroup)
 			}
-			// 直接将分组字段更新为分组翻译内容
 			*v.ApiGroup = group[*v.ApiGroup]
 		}
 		resp.Data.Data = append(resp.Data.Data,
@@ -67,7 +63,7 @@ func (l *GetApiListLogic) GetApiList(req *types.ApiListReq) (resp *types.ApiList
 				Path:        v.Path,
 				Trans:       l.svcCtx.Trans.Trans(l.ctx, *v.Description),
 				Description: v.Description,
-				Group:       v.ApiGroup, // 根据页大小决定显示原本字段还是分组字段
+				Group:       v.ApiGroup,
 				Method:      v.Method,
 			})
 	}
---
+++ api/internal/logic/user/reset_password_by_email_logic.go
@@ -0,0 +1,67 @@
+package user
+
+import (
+	"context"
+	"github.com/suyuan32/simple-admin-common/i18n"
+	"github.com/suyuan32/simple-admin-core/rpc/types/core"
+	"github.com/zeromicro/go-zero/core/errorx"
+
+	"github.com/suyuan32/simple-admin-core/api/internal/svc"
+	"github.com/suyuan32/simple-admin-core/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ResetPasswordByEmailLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewResetPasswordByEmailLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ResetPasswordByEmailLogic {
+	return &ResetPasswordByEmailLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx}
+}
+
+func (l *ResetPasswordByEmailLogic) ResetPasswordByEmail(req *types.ResetPasswordByEmailReq) (resp *types.BaseMsgResp, err error) {
+	captchaData, err := l.svcCtx.Redis.Get("CAPTCHA_" + req.Email)
+	if err != nil {
+		logx.Errorw("failed to get captcha data in redis for email validation", logx.Field("detail", err),
+			logx.Field("data", req))
+		return nil, errorx.NewCodeInvalidArgumentError(i18n.Failed)
+	}
+
+	if captchaData == req.Captcha {
+		userData, err := l.svcCtx.CoreRpc.GetUserList(l.ctx, &core.UserListReq{
+			Page:     1,
+			PageSize: 1,
+			Email:    &req.Email,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		if userData.Total == 0 {
+			return nil, errorx.NewCodeInvalidArgumentError("login.userNotExist")
+		}
+
+		result, err := l.svcCtx.CoreRpc.UpdateUser(l.ctx, &core.UserInfo{Id: userData.Data[0].Id, Password: &req.Password})
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = l.svcCtx.Redis.Del("CAPTCHA_" + req.Email)
+		if err != nil {
+			logx.Errorw("failed to delete captcha in redis", logx.Field("detail", err))
+		}
+
+		return &types.BaseMsgResp{Msg: l.svcCtx.Trans.Trans(l.ctx, result.Msg)}, nil
+	}
+
+	return &types.BaseMsgResp{
+		Code: 0,
+		Msg:  l.svcCtx.Trans.Trans(l.ctx, "login.wrongCaptcha"),
+	}, nil
+}
