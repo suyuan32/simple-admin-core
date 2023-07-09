3857b6206ae26dcad85b55896fe34cdf36e7b388
Ryan SU <yuansu.china.work@gmail.com>
2023-07-09 17:38:02 +0800

    chore: upgrade deps

--- api/core.go
+++ api/core.go
@@ -5,7 +5,7 @@
 //		Schemes: http, https
 //		Host: localhost:9100
 //		BasePath: /
-//		Version: 1.0.12
+//		Version: 1.0.13
 //		Contact: yuansu.china.work@gmail.com
 //		SecurityDefinitions:
 //		  Token:
