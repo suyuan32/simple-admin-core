e18ea2bf91cdba5f28892ffe200b7d1d48c3c4de
Ryan Su <yuansu.china.work@gmail.com>
2023-11-29 14:39:45 +0800

    feat: department Id in token

--- api/internal/logic/publicuser/login_logic.go
+++ api/internal/logic/publicuser/login_logic.go
@@ -51,7 +51,7 @@ func (l *LoginLogic) Login(req *types.LoginReq) (resp *types.LoginResp, err erro
 
 		token, err := jwt.NewJwtToken(l.svcCtx.Config.Auth.AccessSecret, time.Now().Unix(),
 			l.svcCtx.Config.Auth.AccessExpire, jwt.WithOption("userId", user.Id), jwt.WithOption("roleId",
-				strings.Join(user.RoleCodes, ",")))
+				strings.Join(user.RoleCodes, ",")), jwt.WithOption("deptId", user.DepartmentId))
 		if err != nil {
 			return nil, err
 		}
